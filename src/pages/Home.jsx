7839a59c307e47eab5e271918beaa6502c146593
Sergey87 <serge.techno@gmail.com>
2024-09-09 22:06:11 +0300

    added top doctors component

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,13 @@
 import Header from '../components/Header';
 import SpecialityMenu from '../components/SpecialityMenu';
+import TopDoctors from '../components/TopDoctors';
 
 const Home = () => {
   return (
     <div className=''>
       <Header />
       <SpecialityMenu />
+      <TopDoctors />
     </div>
   );
 };
