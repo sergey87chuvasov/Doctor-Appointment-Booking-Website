9ec114ea96d49218698b6b132cd13444dfa83aab
Sergey87 <serge.techno@gmail.com>
2024-09-09 20:54:17 +0300

    added find by components

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,11 @@
 import Header from '../components/Header';
+import SpecialityMenu from '../components/SpecialityMenu';
 
 const Home = () => {
   return (
     <div className=''>
       <Header />
+      <SpecialityMenu />
     </div>
   );
 };
