fad5fc7171138a34ec5c190a429b7d51ec831a9f
Sergey87 <serge.techno@gmail.com>
2024-09-12 00:23:05 +0300

    added work with appointment page - 1 part

--- src/pages/Appointment.jsx
+++ src/pages/Appointment.jsx
@@ -1,5 +1,68 @@
+import { useContext, useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import { AppContext } from '../context/AppContext';
+import { assets } from '../assets/assets';
+
 const Appointment = () => {
-  return <div className=''></div>;
+  const { docId } = useParams();
+  const { doctors } = useContext(AppContext);
+
+  const [docInfo, setDocInfo] = useState(null);
+
+  const fetchDocInfo = async () => {
+    const docInfo = doctors.find((doc) => doc._id === docId);
+    setDocInfo(docInfo);
+    // console.log(docInfo);
+  };
+
+  useEffect(() => {
+    fetchDocInfo();
+  }, [doctors, docId]);
+
+  return (
+    docInfo && (
+      <div className=''>
+        <div className='flex flex-col sm:flex-row gap-4'>
+          <div>
+            <img
+              className='bg-primary w-full sm:max-w-72 rounded-lg '
+              src={docInfo.image}
+              alt='img pic'
+            />
+          </div>
+
+          <div className='flex-1 border border-gray-400 rounded-lg p-8 py-7 bg-white mx-2 sm:mx-0 mt-[-80px] sm:mt-0'>
+            <p className='flex items-center gap-2 text-2xl font-medium text-gray-900'>
+              {docInfo.name}
+              <img className='w-5' src={assets.verified_icon} alt='icon pic' />
+            </p>
+            <div className='flex items-center gap-2 text-sm mt-1 text-gray-600'>
+              <p>
+                {docInfo.degree} - {docInfo.speciality}
+              </p>
+              <button className='py-0.5 px-2 border text-xs rounded-full'>
+                {docInfo.experience}
+              </button>
+            </div>
+
+            <div>
+              <p className='flex items-center gap-1 text-sm font-medium text-gray-900 mt-3'>
+                About
+                <img src={assets.info_icon} alt='icon pic' />
+              </p>
+              <p className='text-sm text-gray-500 max-w-[700px] mt-1'>
+                {docInfo.about}
+              </p>
+            </div>
+            <p>
+              Appointment fee:
+              <span>{docInfo.fees}</span>
+            </p>
+          </div>
+        </div>
+      </div>
+    )
+  );
 };
 
 export default Appointment;
