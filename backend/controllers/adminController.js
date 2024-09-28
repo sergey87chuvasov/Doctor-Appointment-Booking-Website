5800ff6b618704a2376ca654ecbfb43827a23767
Sergey87 <serge.techno@gmail.com>
2024-09-28 21:56:35 +0300

    work with adminController

--- backend/controllers/adminController.js
+++ backend/controllers/adminController.js
@@ -1,3 +1,8 @@
+import validator from 'validator';
+import bcrypt from 'bcrypt';
+import { v2 as cloudinary } from 'cloudinary';
+import doctorModel from '../models/doctorModel.js';
+
 // api for add doctor
 const addDoctor = async (req, res) => {
   try {
@@ -12,7 +17,72 @@ const addDoctor = async (req, res) => {
       fees,
       address,
     } = req.body;
-  } catch (error) {}
+    const imageFile = req.file;
+
+    // check for all data to add doctor
+
+    if (
+      !name ||
+      !email ||
+      !password ||
+      !speciality ||
+      !degree ||
+      !experience ||
+      !about ||
+      !fees ||
+      !address
+    ) {
+      return res.json({ success: false, message: 'Missing Details' });
+    }
+
+    // validate email format
+    if (!validator.isEmail(email)) {
+      return res.json({
+        success: false,
+        message: 'Please enter a valid email',
+      });
+    }
+
+    //valid strong pass
+    if (password.length < 8) {
+      return res.json({
+        success: false,
+        message: 'Please enter a strong password',
+      });
+    }
+
+    // hashing doctor pass
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    //upload image to cloudinary
+    const imageUpload = await cloudinary.uploader.upload(imageFile.path, {
+      resource_type: 'image',
+    });
+    const imageUrl = imageUpload.secure_url;
+
+    const doctorData = {
+      name,
+      email,
+      image: imageUrl,
+      password: hashedPassword,
+      speciality,
+      degree,
+      experience,
+      about,
+      fees,
+      address: JSON.parse(address),
+      date: Date.now(),
+    };
+
+    const newDoctor = new doctorModel(doctorData);
+    await newDoctor.save();
+
+    res.json({ success: true, message: 'Doctor Added' });
+  } catch (error) {
+    console.log(error);
+    res.json({ success: false, message: error.message });
+  }
 };
 
 export { addDoctor };
