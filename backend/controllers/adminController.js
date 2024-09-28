8e7921b8e4736d8280c5570c9d451a3f386e227e
Sergey87 <serge.techno@gmail.com>
2024-09-28 19:59:33 +0300

    work with controllers and middlewares

---
+++ backend/controllers/adminController.js
@@ -0,0 +1,18 @@
+// api for add doctor
+const addDoctor = async (req, res) => {
+  try {
+    const {
+      name,
+      email,
+      password,
+      speciality,
+      degree,
+      experience,
+      about,
+      fees,
+      address,
+    } = req.body;
+  } catch (error) {}
+};
+
+export { addDoctor };
--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ import cors from 'cors';
 import 'dotenv/config';
 import connectDB from './config/mongodb.js';
 import connectCloudinary from './config/cloudinary.js';
+import adminRouter from './routes/adminRoute.js';
 
 // app config
 const app = express();
@@ -15,6 +16,8 @@ app.use(express.json());
 app.use(cors());
 
 // api endpoint
+app.use('/api/admin', adminRouter); //localhost: 4000/api/admin
+
 app.get('/', (req, res) => {
   res.send('Api working !!!');
 });
