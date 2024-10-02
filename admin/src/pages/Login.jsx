90359087100cde177338017c88bfe5906547a5c6
Sergey87 <serge.techno@gmail.com>
2024-10-02 23:45:37 +0300

    work with admin panel - 2 part

--- admin/src/pages/Login.jsx
+++ admin/src/pages/Login.jsx
@@ -1,5 +1,62 @@
+import { useState } from 'react';
+import { assets } from '../assets/assets';
+import { useContext } from 'react';
+import { AdminContext } from '../context/AdminContext';
+
 const Login = () => {
-  return <div className=''></div>;
+  const [state, setState] = useState('Admin');
+
+  const { setAToken, backendUrl } = useContext(AdminContext);
+
+  return (
+    <form className='min-h-[80vh] flex items-center'>
+      <div className='flex flex-col gap-3 m-auto items-start p-8 min-w-[340px] sm:min-w-96 border rounded-xl text-[#5e5e5e] text-sm shadow-lg'>
+        <p className='text-2xl font-semibold m-auto'>
+          <span className='text-primary'>{state}</span> Login
+        </p>
+        <div className='w-full'>
+          <p>Email</p>
+          <input
+            className='border border-[#DADADA] rounded w-full p-2 mt-1'
+            type='email'
+            required
+          />
+        </div>
+        <div className='w-full'>
+          <p>Password</p>
+          <input
+            className='border border-[#DADADA] rounded w-full p-2 mt-1'
+            type='password'
+            required
+          />
+        </div>
+        <button className='bg-primary text-white w-full py-2 rounded-md text-base'>
+          Login
+        </button>
+        {state === 'Admin' ? (
+          <p>
+            Doctor Login?{' '}
+            <span
+              onClick={() => setState('Doctor')}
+              className='text-primary underline cursor-pointer'
+            >
+              Click here
+            </span>
+          </p>
+        ) : (
+          <p>
+            Admin Login?{' '}
+            <span
+              onClick={() => setState('Admin')}
+              className='text-primary underline cursor-pointer'
+            >
+              Click here
+            </span>
+          </p>
+        )}
+      </div>
+    </form>
+  );
 };
 
 export default Login;
