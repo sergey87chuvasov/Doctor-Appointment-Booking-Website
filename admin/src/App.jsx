88ab4febea5c2e784e63ff81129ca0f812cf43d2
Sergey87 <serge.techno@gmail.com>
2024-10-04 00:07:46 +0300

    work with admin panel - 5 part

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -7,6 +7,9 @@ import Navbar from './components/Navbar';
 import Sidebar from './components/Sidebar';
 import { Route, Routes } from 'react-router-dom';
 import Dashboard from './pages/Admin/Dashboard';
+import AllAppointments from './pages/Admin/AllApointments';
+import AddDoctor from './pages/Admin/AddDoctor';
+import DoctorsList from './pages/Admin/DoctorsList';
 
 const App = () => {
   const { aToken } = useContext(AdminContext);
@@ -18,7 +21,10 @@ const App = () => {
         <Sidebar />
         <Routes>
           <Route path='/' element={<></>} />
-          <Route path='/admin-dshboard' element={<Dashboard />} />
+          <Route path='/admin-dashboard' element={<Dashboard />} />
+          <Route path='/all-appointments' element={<AllAppointments />} />
+          <Route path='/add-doctor' element={<AddDoctor />} />
+          <Route path='/doctor-list' element={<DoctorsList />} />
         </Routes>
       </div>
     </div>
--- admin/src/pages/Admin/AddDoctor.jsx
+++ admin/src/pages/Admin/AddDoctor.jsx
@@ -1,5 +1,42 @@
+import { assets } from '../../assets/assets';
+
 const AddDoctor = () => {
-  return <div></div>;
+  return (
+    <form>
+      <p>Add Doctor</p>
+
+      <div>
+        <div>
+          <label htmlFor='doc-img'>
+            <img src={assets.upload_area} alt='img pic' />
+          </label>
+          <input type='file' id='doc-img' hidden />
+          <p>
+            Upload doctor <br /> picture
+          </p>
+        </div>
+
+        <div>
+          <div>
+            <div>
+              <p>Doctor name</p>
+              <input type='text' placeholder='Name' required />
+            </div>
+
+            <div>
+              <p>Doctor email</p>
+              <input type='email' placeholder='Email' required />
+            </div>
+
+            <div>
+              <p>Doctor Password</p>
+              <input type='password' placeholder='Password' required />
+            </div>
+          </div>
+        </div>
+      </div>
+    </form>
+  );
 };
 
 export default AddDoctor;
