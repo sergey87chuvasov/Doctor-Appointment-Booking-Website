3be5583ec24fa9c7a54d01e269615f68587a89d2
Sergey87 <serge.techno@gmail.com>
2024-10-03 18:46:19 +0300

    work with admin panel - 3 part

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,10 +1,22 @@
+import { useContext } from 'react';
 import Login from './pages/Login';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import { AdminContext } from './context/AdminContext';
+import Navbar from './components/Navbar';
 
 const App = () => {
-  return (
-    <div className=''>
-      <Login />
+  const { aToken } = useContext(AdminContext);
+  return aToken ? (
+    <div className='bg-[#f8f9fd]'>
+      <ToastContainer />
+      <Navbar />
     </div>
+  ) : (
+    <>
+      <Login />
+      <ToastContainer />
+    </>
   );
 };
 
---
+++ admin/src/components/Navbar.jsx
@@ -0,0 +1,14 @@
+import { assets } from '../assets/assets';
+
+const Navbar = () => {
+  return (
+    <div>
+      <div>
+        <img src={assets.admin_logo} alt='logo pic' />
+        <p></p>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
