b9b51f9c36da6527766ecb0d6e2ca89f7b05243c
Sergey Chv <serge.techno@gmail.com>
2024-10-05 18:31:30 +0300

    finished Add doctor functionality in Admin Panel

--- admin/src/pages/Admin/AddDoctor.jsx
+++ admin/src/pages/Admin/AddDoctor.jsx
@@ -1,7 +1,8 @@
 import { useContext, useState } from 'react';
 import { assets } from '../../assets/assets';
 import { AdminContext } from '../../context/AdminContext'
-import { toast } from 'react-toastify'
+import { toast } from 'react-toastify';
+import axios from 'axios';
 
 const AddDoctor = () => {
 
@@ -27,9 +28,44 @@ const AddDoctor = () => {
       if(!docImg) {
         return toast.error('Image Not Selected')
       }
+
+      const formData = new FormData();
+      formData.append('image', docImg);
+      formData.append('name', name);
+      formData.append('email', email);
+      formData.append('password', password);
+      formData.append('experience', experience);
+      formData.append('fees', Number(fees));
+      formData.append('about', about);
+      formData.append('speciality', speciality);
+      formData.append('degree', degree);
+      formData.append('address', JSON.stringify({line1: address1, line2: address2}));
+
+      //console lig formdata
+    formData.forEach((value, key) =>{
+      console.log(`${key} : ${value}`)
+    })
+
+    const { data } = await axios.post(backendUrl + '/api/admin/add-doctor', formData, {headers: {aToken}})
+
+    if(data.success) {
+      toast.success(data.message)
+      setDocImg(false);
+      setName('');
+      setPassword('');
+      setEmail('');
+      setAddress1('');
+      setAddress2('');
+      setDegree('');
+      setAbout('');
+      setFees('');
+    } else {
+      toast.error(data.message)
+    }
     }
     catch(error){
-      console.log(error)
+      console.log(error);
+      toast.error(error.message)
     }
   }
 
