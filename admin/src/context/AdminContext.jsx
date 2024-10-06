191be1de299b6ddf72a169ee1c6247f9a4ae1ce4
Sergey87 <serge.techno@gmail.com>
2024-10-06 23:20:39 +0300

    finished Display all doctors list in Admin panel

--- admin/src/context/AdminContext.jsx
+++ admin/src/context/AdminContext.jsx
@@ -8,7 +8,7 @@ const AdminContextProvider = (props) => {
   const [aToken, setAToken] = useState(
     localStorage.getItem('aToken') ? localStorage.getItem('aToken') : ''
   );
-  const [doctors, setDoctors] = useState('');
+  const [doctors, setDoctors] = useState([]);
 
   const backendUrl = import.meta.env.VITE_BACKEND_URL;
 
@@ -22,7 +22,26 @@ const AdminContextProvider = (props) => {
 
       if (data.success) {
         setDoctors(data.doctors);
-        console.log(data.doctors);
+        // console.log(data.doctors);
+      } else {
+        toast.error(data.message);
+      }
+    } catch (error) {
+      toast.error(error.message);
+    }
+  };
+
+  const changeAvailability = async (docId) => {
+    try {
+      const { data } = await axios.post(
+        backendUrl + '/api/admin/change-availability',
+        { docId },
+        { headers: { aToken } }
+      );
+
+      if (data.success) {
+        toast.success(data.message);
+        getAllDoctors();
       } else {
         toast.error(data.message);
       }
@@ -37,6 +56,7 @@ const AdminContextProvider = (props) => {
     backendUrl,
     doctors,
     getAllDoctors,
+    changeAvailability,
   };
 
   return (
