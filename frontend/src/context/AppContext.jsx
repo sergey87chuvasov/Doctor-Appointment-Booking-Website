d911a124754e671d977445b91d91f103386ed1c0
Sergey87 <serge.techno@gmail.com>
2024-10-08 01:35:35 +0300

    finished Login and Registration system

--- frontend/src/context/AppContext.jsx
+++ frontend/src/context/AppContext.jsx
@@ -8,12 +8,11 @@ export const AppContext = createContext();
 const AppContextProvider = (props) => {
   const currencySymbol = ' $';
   const backendUrl = import.meta.env.VITE_BACKEND_URL;
-  const [doctors, setDoctors] = useState([]);
 
-  const value = {
-    doctors,
-    currencySymbol,
-  };
+  const [doctors, setDoctors] = useState([]);
+  const [token, setToken] = useState(
+    localStorage.getItem('token') ? localStorage.getItem('token') : false
+  );
 
   const getDoctorsData = async () => {
     try {
@@ -28,6 +27,14 @@ const AppContextProvider = (props) => {
       console.log(error);
       toast.error(error.message);
     }
+  };
+
+  const value = {
+    doctors,
+    currencySymbol,
+    token,
+    setToken,
+    backendUrl,
   };
 
   useEffect(() => {
--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,6 +1,14 @@
-import { useState } from 'react';
+import { useContext, useEffect, useState } from 'react';
+import { AppContext } from '../context/AppContext';
+import axios from 'axios';
+import { toast } from 'react-toastify';
+import { useNavigate } from 'react-router-dom';
 
 const Login = () => {
+  const { backendUrl, token, setToken } = useContext(AppContext);
+
+  const navigate = useNavigate();
+
   const [state, setState] = useState('Sign Up');
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
@@ -8,7 +16,45 @@ const Login = () => {
 
   const onSubmitHandler = async (event) => {
     event.preventDefault();
+
+    try {
+      if (state === 'Sign Up') {
+        const { data } = await axios.post(backendUrl + '/api/user/register', {
+          name,
+          password,
+          email,
+        });
+
+        if (data.success) {
+          localStorage.setItem('token', data.token);
+          setToken(data.token);
+        } else {
+          toast.error(data.message);
+        }
+      } else {
+        const { data } = await axios.post(backendUrl + '/api/user/login', {
+          password,
+          email,
+        });
+
+        if (data.success) {
+          localStorage.setItem('token', data.token);
+          setToken(data.token);
+        } else {
+          toast.error(data.message);
+        }
+      }
+    } catch (error) {
+      console.log(error);
+      toast.error(error.message);
+    }
   };
+
+  useEffect(() => {
+    if (token) {
+      navigate('/');
+    }
+  }, [token]);
 
   return (
     <form onSubmit={onSubmitHandler} className='min-h-[80vh] flex items-center'>
@@ -52,7 +98,10 @@ const Login = () => {
             required
           />
         </div>
-        <button className='bg-primary text-white w-full py-2 rounded-md text-base'>
+        <button
+          type='submit'
+          className='bg-primary text-white w-full py-2 rounded-md text-base'
+        >
           {state === 'Sign Up' ? 'Create Account' : 'Login'}
         </button>
         {state === 'Sign Up' ? (
