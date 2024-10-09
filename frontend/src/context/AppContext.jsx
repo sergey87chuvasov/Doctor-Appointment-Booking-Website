ade3b0c09159dd3dd09d10042a831ff8be95459d
Sergey87 <serge.techno@gmail.com>
2024-10-09 23:13:43 +0300

    Display and update patient profile data on website - 2 part

--- frontend/src/context/AppContext.jsx
+++ frontend/src/context/AppContext.jsx
@@ -13,6 +13,7 @@ const AppContextProvider = (props) => {
   const [token, setToken] = useState(
     localStorage.getItem('token') ? localStorage.getItem('token') : false
   );
+  const [userData, setUserData] = useState(false);
 
   const getDoctorsData = async () => {
     try {
@@ -29,17 +30,45 @@ const AppContextProvider = (props) => {
     }
   };
 
+  const loadUserProfileData = async () => {
+    try {
+      const { data } = await axios.get(backendUrl + '/api/user/get-profile', {
+        headers: { token },
+      });
+
+      if (data.success) {
+        setUserData(data.userData);
+      } else {
+        toast.error(data.message);
+      }
+    } catch (error) {
+      console.log(error);
+      toast.error(error.message);
+    }
+  };
+
   const value = {
     doctors,
     currencySymbol,
     token,
     setToken,
     backendUrl,
+    userData,
+    setUserData,
+    loadUserProfileData,
   };
 
   useEffect(() => {
     getDoctorsData();
   }, []);
+
+  useEffect(() => {
+    if (token) {
+      loadUserProfileData();
+    } else {
+      setUserData(false);
+    }
+  }, [token]);
 
   return (
     <AppContext.Provider value={value}>{props.children}</AppContext.Provider>
