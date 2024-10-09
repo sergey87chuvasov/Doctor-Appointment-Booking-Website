3e8c5e59dd425148bacb16e8a3468bafd1493dd9
Sergey87 <serge.techno@gmail.com>
2024-10-10 00:29:54 +0300

    finished Display and update patient profile data on website

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -6,7 +6,7 @@ import { AppContext } from '../context/AppContext';
 const Navbar = () => {
   const navigate = useNavigate();
 
-  const { token, setToken } = useContext(AppContext);
+  const { token, setToken, userData } = useContext(AppContext);
 
   const [showMenu, setShowMenu] = useState(false);
   // const [token, setToken] = useState(true);
@@ -43,11 +43,11 @@ const Navbar = () => {
         </NavLink>
       </ul>
       <div className='flex items-center gap-4'>
-        {token ? (
+        {token && userData ? (
           <div className='flex items-center gap-2 cursor-pointer group relative'>
             <img
               className='w-8 rounded-full'
-              src={assets.profile_pic}
+              src={userData.image}
               alt='profile img'
             />
             <img className='w-2.5' src={assets.dropdown_icon} alt='icon pic' />
--- frontend/src/pages/MyProfile.jsx
+++ frontend/src/pages/MyProfile.jsx
@@ -1,6 +1,8 @@
 import { useContext, useState } from 'react';
-// import { assets } from '../assets/assets';
+import { assets } from '../assets/assets';
 import { AppContext } from '../context/AppContext';
+import axios from 'axios';
+import { toast } from 'react-toastify';
 
 const MyProfile = () => {
   // const [userData, setUserData] = useState({
@@ -22,12 +24,65 @@ const MyProfile = () => {
   const [isEdit, setIsEdit] = useState(false);
   const [image, setImage] = useState(false);
 
-  const updateUserProfileData = async () => {};
+  const updateUserProfileData = async () => {
+    try {
+      const formData = new FormData();
+
+      formData.append('name', userData.name);
+      formData.append('phone', userData.phone);
+      formData.append('address', JSON.stringify(userData.address));
+      formData.append('gender', userData.gender);
+      formData.append('dob', userData.dob);
+
+      image && formData.append('image', image);
+
+      const { data } = await axios.post(
+        backendUrl + '/api/user/update-profile',
+        formData,
+        { headers: { token } }
+      );
+
+      if (data.success) {
+        toast.success(data.message);
+        await loadUserProfileData();
+        setIsEdit(false);
+        setImage(false);
+      } else {
+        toast.error(data.message);
+      }
+    } catch (error) {
+      console.log(error);
+      toast.error(error.message);
+    }
+  };
 
   return (
     userData && (
       <div className='max-w-lg flex flex-col gap-2 text-sm'>
-        <img className='w-36 rounded' src={userData.image} alt='img pic' />
+        {isEdit ? (
+          <label htmlFor='image'>
+            <div className='inline-block relative cursor-pointer'>
+              <img
+                className='w-36 rounded opacity-75'
+                src={image ? URL.createObjectURL(image) : userData.image}
+                alt='img pic'
+              />
+              <img
+                className='w-10 absolute bottom-12 right-12'
+                src={image ? '' : assets.upload_icon}
+                alt='img pic'
+              />
+            </div>
+            <input
+              onChange={(e) => setImage(e.target.files[0])}
+              type='file'
+              id='image'
+              hidden
+            />
+          </label>
+        ) : (
+          <img className='w-36 rounded' src={userData.image} alt='img pic' />
+        )}
 
         {isEdit ? (
           <input
@@ -135,7 +190,7 @@ const MyProfile = () => {
           {isEdit ? (
             <button
               className='border border-primary px-8 py-2 rounded-full hover:bg-primary hover:text-white transition-all'
-              onClick={() => setIsEdit(false)}
+              onClick={updateUserProfileData}
             >
               Save information
             </button>
