f5550bf8817b40883a8708017eaeb62f7641bcc5
Sergey87 <serge.techno@gmail.com>
2024-10-22 23:19:32 +0300

    Work with Display and Update profile data In Doctor panel - 3 part

--- admin/src/components/Navbar.jsx
+++ admin/src/components/Navbar.jsx
@@ -2,9 +2,11 @@ import { useContext } from 'react';
 import { assets } from '../assets/assets';
 import { AdminContext } from '../context/AdminContext';
 import { useNavigate } from 'react-router-dom';
+import { DoctorContext } from '../context/DoctorContext';
 
 const Navbar = () => {
   const { aToken, setAToken } = useContext(AdminContext);
+  const { dToken, setDToken } = useContext(DoctorContext);
 
   const navigate = useNavigate();
 
